515457b065b1d865f3d1d5d150c4acdb2fe5d665
Ignacio <ignaciosabiajes@gmail.com>
2023-07-07 13:07:29 -0300

    more minor changes

--- src/app/countryPage/page.tsx
+++ src/app/countryPage/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react'
 import './Country.css';
+import Link from 'next/link';
 
 function Page() {
 
@@ -28,7 +29,7 @@ function Page() {
     /*Here should go the NavBar to toggle between Dark and Light mode. */
     <main>
         { /*Here Goes the main section of the County Information.*/}
-        <button>Back</button>
+        <Link href={"/"}><button>Back</button></Link>
         {country ? 
           
         <> 
@@ -36,7 +37,7 @@ function Page() {
             <img  src={country[0].flags.png}/>
           </section>
           <aside className='countryMainInfo'>
-              <h1>Name: {country[0].name.common}</h1>
+              <h1>{country[0].name.common}</h1>
               <p>Population: {country[0].population.toLocaleString('en-US')}</p>
               <p>Region: {country[0].region}</p>
               <p>Sub Region: {country[0].subregion ? country[0].subregion : "none"}</p>
