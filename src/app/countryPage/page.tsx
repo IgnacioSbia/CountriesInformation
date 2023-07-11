606e6e72cb03873945fe63008a85bb35c930bdc8
Ignacio <ignaciosabiajes@gmail.com>
2023-07-11 11:28:02 -0300

    Dark Mode for country done

--- src/app/countryPage/page.tsx
+++ src/app/countryPage/page.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import './Country.css';
 import Link from 'next/link';
-import leftArrow from './CountryImgs/arrow-left-solid.svg';
+import leftArrowDarkMode from './CountryImgs/DarkModeArrow.svg';
+import leftArrowLightMode from './CountryImgs/LightModeArrow.svg'
 import Image from 'next/image';
 function Page() {
 
@@ -28,22 +29,22 @@ function Page() {
 
   return (
     /*Here should go the NavBar to toggle between Dark and Light mode. */
-    <main>
+    <main className='countryMainPageDark'>
         { /*Here Goes the main section of the County Information.*/}
-        <Link href={"/"}><button className='countryGoBackHomeButton'><Image alt='leftArrow' src={leftArrow} width={20}/>Back</button></Link>
+        <Link href={"/"}><button className='countryGoBackHomeButton'><Image alt='leftArrow' src={leftArrowDarkMode} width={20} className='leftArrowButtonImage'/>Back</button></Link>
         {country ? 
           
         <>
           <section className='mainCountrySection'>
             <section className='countryPageImg'>
-              <img  src={country[0].flags.png}/>
+              <img  src={country[0].flags.png} className='countryPageCountryImg'/>
             </section>
             <aside className='countryMainInfo'>
-                <h1>{country[0].name.common}</h1>
-                <p>Population: {country[0].population.toLocaleString('en-US')}</p>
-                <p>Region: {country[0].region}</p>
-                <p>Sub Region: {country[0].subregion ? country[0].subregion : "none"}</p>
-                <p>Currency:</p>
+                <h1 className='countryMainTitleCountry'>{country[0].name.common}</h1>
+                <p className='countryPageInfo'>Population: {country[0].population.toLocaleString('en-US')}</p>
+                <p className='countryPageInfo'>Region: {country[0].region}</p>
+                <p className='countryPageInfo'>Sub Region: {country[0].subregion ? country[0].subregion : "none"}</p>
+                <p className='countryPageInfo'>Currency:</p>
             </aside>
           </section>
         </> 
